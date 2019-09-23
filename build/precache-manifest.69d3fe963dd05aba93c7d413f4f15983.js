83c981b7aa1d164c7780a3ddb09db0a0da221396
Gor Manukyan <gorman93@mail.ru>
2019-09-23 13:18:10 +0400

    add build

---
+++ build/precache-manifest.69d3fe963dd05aba93c7d413f4f15983.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5b1695e30871af8bc7b7e787669462a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c38d4366ec4a9964450d",
+    "url": "/static/css/main.441c7085.chunk.css"
+  },
+  {
+    "revision": "da209c609c5c14006214",
+    "url": "/static/js/2.eae1d299.chunk.js"
+  },
+  {
+    "revision": "c38d4366ec4a9964450d",
+    "url": "/static/js/main.8ad95039.chunk.js"
+  },
+  {
+    "revision": "73fbc06d243c0f96cf2a",
+    "url": "/static/js/runtime~main.527ab2ee.js"
+  }
+]);
